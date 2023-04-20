77fe04c2ed7bad5dbb5ce14861f6884ab3914eb1
endyd1567 <endyd1567@gmail.com>
2023-04-20 20:25:58 +0900

    1. 사용자 추가 기능

--- src/main/java/portalService/test2/user/UserDao.java
+++ src/main/java/portalService/test2/user/UserDao.java
@@ -1,2 +1,44 @@
-package portalService.test2.user;public class UserDao {
+package portalService.test2.user;
+
+import java.sql.*;
+
+import static portalService.test2.connection.ConnectionConst.*;
+
+public class UserDao {
+    public User findById(Long id) throws SQLException {
+        Connection con = DriverManager.getConnection(URL_JEJU, USERNAME_JEJU, PASSWORD_JEJU);
+        String sql = "select id,name,password from userinfo where id=?";
+        PreparedStatement psmt = con.prepareStatement(sql);
+        psmt.setLong(1, id);
+        ResultSet rs = psmt.executeQuery();
+
+        rs.next();
+
+        User user = new User();
+        user.setId(rs.getLong("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+        rs.close();
+        psmt.close();
+        con.close();
+
+        return user;
+    }
+
+    public void insert(User user) throws SQLException {
+        Connection con = DriverManager.getConnection(URL_JEJU, USERNAME_JEJU, PASSWORD_JEJU);
+        String sql = "insert into userinfo(name,password) values(?,?)";
+        PreparedStatement psmt = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+        psmt.setString(1, user.getName());
+        psmt.setString(2,user.getPassword());
+        psmt.executeUpdate();
+
+        ResultSet rs = psmt.getGeneratedKeys();
+        rs.next();
+        user.setId(rs.getLong(1));
+
+        rs.close();
+        psmt.close();
+        con.close();
+    }
 }
--- src/test/java/portalService/test2/user/UserDaoTest.java
+++ src/test/java/portalService/test2/user/UserDaoTest.java
@@ -1,4 +1,45 @@
-import static org.junit.jupiter.api.Assertions.*;
+package portalService.test2.user;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class UserDaoTest {
-  
+
+    @Test
+    public void get() throws SQLException, SQLException {
+
+        Long id = 1l;
+        String name = "umdu";
+        String password = "1234";
+
+        UserDao userDao = new UserDao();
+        User user = userDao.findById(1l);
+
+        assertThat(user.getId()).isEqualTo(id);
+        assertThat(user.getName()).isEqualTo(name);
+        assertThat(user.getPassword()).isEqualTo(password);
+    }
+
+    @Test
+    public void insert() throws SQLException {
+
+        User user = new User();
+        String name = "재봉";
+        String password = "1234";
+        user.setName(name);
+        user.setPassword(password);
+
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+
+        User insertedUser = userDao.findById(user.getId());
+
+        assertThat(insertedUser.getId()).isEqualTo(user.getId());
+        assertThat(insertedUser.getName()).isEqualTo(user.getName());
+        assertThat(insertedUser.getName()).isEqualTo(user.getName());
+    }
+
 }
